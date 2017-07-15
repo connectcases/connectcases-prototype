e852ed621cb6d1ba995ee78ea8aaebbd97c5f6c7
Tom Mortimer-Jones <tom@morty.co.uk>
2017-07-15 13:35:39 +0100

    Use Heroku database when available

--- connectcases/settings.py
+++ connectcases/settings.py
@@ -81,6 +81,10 @@ DATABASES = {
     }
 }
 
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    db_from_env = dj_database_url.config()
+    DATABASES['default'].update(db_from_env
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
