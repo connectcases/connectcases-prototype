fa3225293f998f1b1b8f270a37c30a4b8e47e6f4
Tom Mortimer-Jones <tom@morty.co.uk>
2017-07-16 14:37:29 +0100

    Add Patient to admin

--- connectcases/admin.py
+++ connectcases/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Device
+from .models import Device, Patient
 
 class DeviceAdmin(admin.ModelAdmin):
         pass
 
 admin.site.register(Device, DeviceAdmin)
+
+class PatientAdmin(admin.ModelAdmin):
+        pass
+
+admin.site.register(Patient, PatientAdmin)
